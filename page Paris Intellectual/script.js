9ac827a150728d09c6ab0df620648321e77a673e
Pharmamara <140740925+Pharmamara@users.noreply.github.com>
2024-07-03 19:55:50 +0300

    intellectual fin add

--- page Paris Intellectual/script.js
+++ page Paris Intellectual/script.js
@@ -1,9 +1,8 @@
 initMap();
 
 async function initMap() {
-  await ymaps3.ready;
-
-  const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+  await ymaps.ready;
+  const { YMap, YMapDefaultSchemeLayer } = ymaps;
 
   const map = new YMap(document.getElementById("map"), {
     location: {
@@ -14,3 +13,27 @@ async function initMap() {
 
   map.addChild(new YMapDefaultSchemeLayer());
 }
+
+fetch(
+  "http://api.openweathermap.org/data/2.5/weather?id=2968815&lang=ru&appid=1adb2913bcf2d859d29cdf8f88076f65"
+)
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {
+    //добавляем название города
+    document.querySelector(".weather__city").textContent = data.name;
+    //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
+    document.querySelector(".weather__forecast").innerHTML =
+      Math.round(data.main.temp - 273) + "&deg;";
+    //Добавляем описание погоды
+    document.querySelector(".weather__desc").textContent =
+      data.weather[0]["description"];
+    //Добавляем иконку погоды
+    document.querySelector(
+      ".weather__icon"
+    ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`;
+  })
+  .catch(function () {
+    //Обрабатываем ошибки
+  });
